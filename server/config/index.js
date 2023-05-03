c595a72c7751c3952b727bf7527a3f142821e1bb
Eseosa Emovon <P2508279@my365.dmu.ac.uk>
2023-05-03 17:50:46 +0100

    Initial commit

---
+++ server/config/index.js
@@ -0,0 +1,8 @@
+```js
+module.exports = {
+  SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
+  SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
+  SPOTIFY_REDIRECT_URI: process.env.SPOTIFY_REDIRECT_URI,
+  MONGO_URI: process.env.MONGO_URI,
+};
+```;
